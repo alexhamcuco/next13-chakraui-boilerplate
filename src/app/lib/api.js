da5f199debab3b9c3ac998f8e55a62c1988fc5f4
Diego <diego-jofre@live.com.ar>
2023-08-24 18:28:46 -0300

    cambios en api.js y en el componente main

--- src/app/lib/api.js
+++ src/app/lib/api.js
@@ -1,10 +1,10 @@
-import axios from "axios"
+import axios from "axios";
 
 const api = axios.create({
   baseURL: "http://localhost:3000/api",
 });
 
 export const getMateriales = async () => {
-  const res = await api.get("/materiales");
+  const res = await api.get("/materials");
   return res.data;
 };
--- src/app/main/layout.js
+++ src/app/main/layout.js
@@ -1,14 +1,16 @@
+"use client";
+
 import { Heading, Flex } from "@chakra-ui/react";
 import Navbar from "../navbar/layout";
 
 export const Main = () => {
   return (
     <main>
-      {/* <Navbar />
+      <Navbar />
       <Flex height="100vh" alignItems="center" justifyContent="center">
         <Heading>Install Chakra UI In NextJS 13</Heading>
-      </Flex> */}
+      </Flex>
       lalala
     </main>
   );
-}
+};
