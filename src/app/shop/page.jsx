352aef1ba89c9d53f66815b11ff8ad4a5d3ea569
okashatanoli12345@gmail.com <64710012+OkashaTanoli@users.noreply.github.com>
2023-12-02 01:01:46 +0500

    ui fixes done

--- src/app/shop/page.jsx
+++ src/app/shop/page.jsx
@@ -1,21 +1,31 @@
 'use client'
 
-import React from "react";
+import React, { useState } from "react";
 import './shop.css'
 import Image from "next/image";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/pagination';
+import { useColorMode } from "@chakra-ui/react";
+import { useTheme } from "@emotion/react";
+
+
 
 
 const ShopPage = () => {
+  const { colors } = useTheme();
+
+
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  const [size, setSize] = useState('')
   return (
     <>
       <div className="container">
         <div className="inner-container">
-          <div className="card_outer">
-            <div className="card_main">
+          <div className="card_outer" style={colorMode === 'light' ? { backgroundColor: colors.light.bgSecondary } : { backgroundColor: colors.dark.bgSecondary }}>
+            <div className="card_main" style={colorMode === 'light' ? { backgroundColor: colors.light.bgPrimary, borderColor: colors.light.border } : { backgroundColor: colors.dark.bgPrimary, borderColor: colors.dark.border }}>
               <div className="card_first_div">
                 <h2 className="card_first_div_heading">CUP VERBS</h2>
                 <div className="card_first_div_image_div">
@@ -49,11 +59,19 @@ const ShopPage = () => {
               <div className="card_third_div">
                 <div className="card_third_div_first">
                   <p>Quantity: </p>
-                  <input type="number" />
+                  <input type="number" style={colorMode === 'light' ? { backgroundColor: 'lightgrey' } : { backgroundColor: 'white' }} />
                 </div>
                 <div className="card_third_div_second">
                   <p>T-Shirt</p>
-                  <p>SIZES: S / MD / X / XL</p>
+                  <div style={{ display: 'flex', gap: '15px', alignItems: 'center' }}>
+                    <p>SIZE:</p>
+                    <div style={{ display: 'flex', gap: '10px' }}>
+                      <div style={{ width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '10px', cursor: 'pointer', backgroundColor: size.size === 'S' && size.id === '1' ? 'lightslategray' : 'lightgray' }} onClick={() => setSize({ size: 'S', id: '1' })}>S</div>
+                      <div style={{ width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '10px', cursor: 'pointer', backgroundColor: size.size === 'MD' && size.id === '1' ? 'lightslategray' : 'lightgray' }} onClick={() => setSize({ size: 'MD', id: '1' })}>MD</div>
+                      <div style={{ width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '10px', cursor: 'pointer', backgroundColor: size.size === 'X' && size.id === '1' ? 'lightslategray' : 'lightgray' }} onClick={() => setSize({ size: 'X', id: '1' })}>X</div>
+                      <div style={{ width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '10px', cursor: 'pointer', backgroundColor: size.size === 'XL' && size.id === '1' ? 'lightslategray' : 'lightgray' }} onClick={() => setSize({ size: 'XL', id: '1' })}>XL</div>
+                    </div>
+                  </div>
                 </div>
                 <div className="card_third_div_third">
                   <button>add to basket</button>
@@ -61,8 +79,8 @@ const ShopPage = () => {
               </div>
             </div>
           </div>
-          <div className="card_outer">
-            <div className="card_main">
+          <div className="card_outer" style={colorMode === 'light' ? { backgroundColor: colors.light.bgSecondary } : { backgroundColor: colors.dark.bgSecondary }}>
+            <div className="card_main" style={colorMode === 'light' ? { backgroundColor: colors.light.bgPrimary, borderColor: colors.light.border } : { backgroundColor: colors.dark.bgPrimary, borderColor: colors.dark.border }}>
               <div className="card_first_div">
                 <h2 className="card_first_div_heading">CUP VERBS</h2>
                 <div className="card_first_div_image_div">
@@ -96,11 +114,19 @@ const ShopPage = () => {
               <div className="card_third_div">
                 <div className="card_third_div_first">
                   <p>Quantity: </p>
-                  <input type="number" />
+                  <input type="number" style={colorMode === 'light' ? { backgroundColor: 'lightgrey' } : { backgroundColor: 'white' }} />
                 </div>
                 <div className="card_third_div_second">
                   <p>T-Shirt</p>
-                  <p>SIZES: S / MD / X / XL</p>
+                  <div style={{ display: 'flex', gap: '15px', alignItems: 'center' }}>
+                    <p>SIZE:</p>
+                    <div style={{ display: 'flex', gap: '10px' }}>
+                      <div style={{ width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '10px', cursor: 'pointer', backgroundColor: size.size === 'S' && size.id === '2' ? 'lightslategray' : 'lightgray' }} onClick={() => setSize({ size: 'S', id: '2' })}>S</div>
+                      <div style={{ width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '10px', cursor: 'pointer', backgroundColor: size.size === 'MD' && size.id === '2' ? 'lightslategray' : 'lightgray' }} onClick={() => setSize({ size: 'MD', id: '2' })}>MD</div>
+                      <div style={{ width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '10px', cursor: 'pointer', backgroundColor: size.size === 'X' && size.id === '2' ? 'lightslategray' : 'lightgray' }} onClick={() => setSize({ size: 'X', id: '2' })}>X</div>
+                      <div style={{ width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '10px', cursor: 'pointer', backgroundColor: size.size === 'XL' && size.id === '2' ? 'lightslategray' : 'lightgray' }} onClick={() => setSize({ size: 'XL', id: '2' })}>XL</div>
+                    </div>
+                  </div>
                 </div>
                 <div className="card_third_div_third">
                   <button>add to basket</button>
@@ -108,8 +134,8 @@ const ShopPage = () => {
               </div>
             </div>
           </div>
-          <div className="card_outer">
-            <div className="card_main">
+          <div className="card_outer" style={colorMode === 'light' ? { backgroundColor: colors.light.bgSecondary } : { backgroundColor: colors.dark.bgSecondary }}>
+            <div className="card_main" style={colorMode === 'light' ? { backgroundColor: colors.light.bgPrimary, borderColor: colors.light.border } : { backgroundColor: colors.dark.bgPrimary, borderColor: colors.dark.border }}>
               <div className="card_first_div">
                 <h2 className="card_first_div_heading">CUP VERBS</h2>
                 <div className="card_first_div_image_div">
@@ -143,11 +169,19 @@ const ShopPage = () => {
               <div className="card_third_div">
                 <div className="card_third_div_first">
                   <p>Quantity: </p>
-                  <input type="number" />
+                  <input type="number" style={colorMode === 'light' ? { backgroundColor: 'lightgrey' } : { backgroundColor: 'white' }} />
                 </div>
                 <div className="card_third_div_second">
                   <p>T-Shirt</p>
-                  <p>SIZES: S / MD / X / XL</p>
+                  <div style={{ display: 'flex', gap: '15px', alignItems: 'center' }}>
+                    <p>SIZE:</p>
+                    <div style={{ display: 'flex', gap: '10px' }}>
+                      <div style={{ width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '10px', cursor: 'pointer', backgroundColor: size.size === 'S' && size.id === '3' ? 'lightslategray' : 'lightgray' }} onClick={() => setSize({ size: 'S', id: '3' })}>S</div>
+                      <div style={{ width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '10px', cursor: 'pointer', backgroundColor: size.size === 'MD' && size.id === '3' ? 'lightslategray' : 'lightgray' }} onClick={() => setSize({ size: 'MD', id: '3' })}>MD</div>
+                      <div style={{ width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '10px', cursor: 'pointer', backgroundColor: size.size === 'X' && size.id === '3' ? 'lightslategray' : 'lightgray' }} onClick={() => setSize({ size: 'X', id: '3' })}>X</div>
+                      <div style={{ width: '40px', height: '40px', display: 'flex', justifyContent: 'center', alignItems: 'center', borderRadius: '10px', cursor: 'pointer', backgroundColor: size.size === 'XL' && size.id === '3' ? 'lightslategray' : 'lightgray' }} onClick={() => setSize({ size: 'XL', id: '3' })}>XL</div>
+                    </div>
+                  </div>
                 </div>
                 <div className="card_third_div_third">
                   <button>add to basket</button>
@@ -155,6 +189,7 @@ const ShopPage = () => {
               </div>
             </div>
           </div>
+         
         </div>
       </div>
     </>
