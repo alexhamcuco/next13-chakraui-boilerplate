972a1c32f5a56b38de24f38b51c096206a566433
alexhamcuco <whitebookopen@gmail.com>
2023-12-02 18:12:58 +0100

    permisos post

--- src/app/lib/api.js
+++ src/app/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: `${NEXT_PUBLIC_API_URL}/api`,
 });
 
 export const getMateriales = async () => {
