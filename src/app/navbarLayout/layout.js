b7f4f9cb4ef90a8cb3cdfc0aae9a92fa74373772
alexhamcuco <whitebookopen@gmail.com>
2023-12-02 17:30:17 +0100

    quitar getMateriales

--- src/app/navbarLayout/layout.js
+++ src/app/navbarLayout/layout.js
@@ -1,9 +1,34 @@
-import { getMateriales } from "../lib/api";
+// import { getMateriales } from "../lib/api";
 import Navbar from "../navbar/layout";
 
 const NavbarLayout = async () => {
-  const materiales = await getMateriales();
-
+  // const materiales = await getMateriales();
+const materiales = [
+  {
+    _id: "6563fbad0d8e042d1c79f070",
+    nivel: "principiante",
+    palabrasClave: "ser, estar, verbos, to be",
+    privilegios: true,
+    tipo: "gramaticas",
+    titulo: "ser o estar (parte 1)",
+    urlTitulo: "ser-vs-estar",
+    urlImagen: "https://www.spanishwithalex.com/images/cards/seryestar1.jpg",
+    descripcion:
+      "la explicación definitiva de los verbos ser y estar en español",
+  },
+  {
+    _id: "6563fbad0d8e042d1c79f070",
+    nivel: "principiante",
+    palabrasClave: "ser, estar, verbos, to be",
+    privilegios: true,
+    tipo: "gramaticas",
+    titulo: "ser o estar (parte 1)",
+    urlTitulo: "ser-vs-estar",
+    urlImagen: "https://www.spanishwithalex.com/images/cards/seryestar1.jpg",
+    descripcion:
+      "la explicación definitiva de los verbos ser y estar en español",
+  },
+];
   return <Navbar materiales={materiales} />;
 };
 
