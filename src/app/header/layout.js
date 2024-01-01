f85e59c994f4bd0d5672ad564f5fd4c9c28baced
alexhamcuco <whitebookopen@gmail.com>
2024-01-01 15:37:46 +0100

    gap

--- src/app/header/layout.js
+++ src/app/header/layout.js
@@ -74,18 +74,18 @@ const Header = () => {
           top="-20px"
           py={8}
         >
-          <Flex gap={{ base: 9 }} width={"60%"} justifyContent={"space-around"}>
+          <Flex gap={{ base: 4 }} width={"60%"} justifyContent={"space-around"}>
             <Flex direction={"column"}>
               <Text fontSize={{ base: "1xl", sm: "1xl"}}>Sessions</Text>
-              <Text fontSize={{ base: "4xl", sm: "6xl"}}>10</Text>
+              <Text fontSize={{ base: "1xl", sm: "6xl"}}>10</Text>
             </Flex>
             <Flex direction={"column"}>
               <Text fontSize={{ base: "1xl", sm: "1xl"}}>Lessons</Text>
-              <Text fontSize={{ base: "4xl", sm: "6xl"}}>200</Text>
+              <Text fontSize={{ base: "1xl", sm: "6xl"}}>200</Text>
             </Flex>
             <Flex direction={"column"}>
               <Text fontSize={{ base: "1xl", sm: "1xl"}}>Podcasts</Text>
-              <Text fontSize={{ base: "4xl", sm: "6xl"}}>100</Text>
+              <Text fontSize={{ base: "1xl", sm: "6xl"}}>100</Text>
             </Flex>
           </Flex>
           <Divider
