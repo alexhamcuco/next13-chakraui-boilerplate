58f06b27376a68881bde3eb7b788f2ed861c89dd
Diego <diego-jofre@live.com.ar>
2024-01-13 00:25:01 -0300

    ajustes en HorizontalCardIlustrations

--- src/app/horizontalCardIlustrations/layout.js
+++ src/app/horizontalCardIlustrations/layout.js
@@ -1,6 +1,14 @@
 "use client";
 
-import { Flex, Image, Box, Text, useColorModeValue } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Flex,
+  Heading,
+  Image,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
 import { useTheme } from "@emotion/react";
 
 const HorizontalCardIlustrations = () => {
@@ -12,38 +20,77 @@ const HorizontalCardIlustrations = () => {
   ];
 
   return (
-    <Box
-      bg={useColorModeValue(colors.light.bgPrimary, colors.dark.bgPrimary)}
-      p="4"
-    >
-      <Flex
-        flexDirection="row"
-        borderWidth="1px"
-        borderRadius="lg"
-        overflow="hidden"
-        boxShadow="2xl"
-        justifyContent="space-around"
-        alignItems="center"
-        maxW="700px"
-        mx="auto"
-        mt="40px"
-        mb="40px"
-        p="4"
-      >
-        {imageAndText.map(({ src, text }, index) => (
-          <Box key={index} w="100%" p="4">
-            <Image
-              src={src}
-              alt={`Illustration ${index + 1}`}
-              w="70%"
-              h="auto"
-            />
-            <Text textAlign="center" mt="2" fontWeight="bold">
-              {text}
+    <Box bg={useColorModeValue(colors.light.bgPrimary, colors.dark.bgPrimary)}>
+      <Container my="16" maxW={{ base: "container.sm", md: "6xl" }}>
+        <Flex
+          mb="8"
+          direction={"column"}
+          alignItems={"center"}
+          textAlign={"center"}
+        >
+          <Text pb="4" color="red">
+            REASONS
+          </Text>
+          <Heading fontSize="3xl" pb="4">
+            Why choose Spanish with Alex
+          </Heading>
+        </Flex>
+        <Flex
+          flexDirection={{ base: "column", md: "row" }}
+          justifyContent={{ base: "center", md: "space-between" }}
+        >
+          <Flex
+            width={{ base: "100%", md: "30%" }}
+            mb={12}
+            direction={"column"}
+            alignItems={"center"}
+            textAlign={"center"}
+            gap={4}
+          >
+            <Image src={"/images/Illustration_1.svg"} alt="" width={224} />
+            <Heading fontSize="2xl">Placeholder</Heading>
+            <Text>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Provident porro nobis earum error cum suscipit dignissimos tenetur
+              commodi nisi. Alias numquam reiciendis quos quibusdam sunt, odio
+              ducimus commodi quas dolore?
             </Text>
-          </Box>
-        ))}
-      </Flex>
+          </Flex>
+          <Flex
+            width={{ base: "100%", md: "30%" }}
+            mb={12}
+            direction={"column"}
+            alignItems={"center"}
+            textAlign={"center"}
+            gap={4}
+          >
+            <Image src={"/images/Illustration_2.svg"} alt="" width={224} />
+            <Heading fontSize="2xl">Placeholder</Heading>
+            <Text>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Provident porro nobis earum error cum suscipit dignissimos tenetur
+              commodi nisi. Alias numquam reiciendis quos quibusdam sunt, odio
+              ducimus commodi quas dolore?
+            </Text>
+          </Flex>
+          <Flex
+            width={{ base: "100%", md: "30%" }}
+            direction={"column"}
+            alignItems={"center"}
+            textAlign={"center"}
+            gap={4}
+          >
+            <Image src={"/images/Illustration_3.svg"} alt="" width={224} />
+            <Heading fontSize="2xl">Placeholder</Heading>
+            <Text>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Provident porro nobis earum error cum suscipit dignissimos tenetur
+              commodi nisi. Alias numquam reiciendis quos quibusdam sunt, odio
+              ducimus commodi quas dolore?
+            </Text>
+          </Flex>
+        </Flex>
+      </Container>
     </Box>
   );
 };
